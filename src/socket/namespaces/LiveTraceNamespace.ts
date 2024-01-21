6ff5e0d4a1cc5fe55136b4b7eef6e9c4c9624025
Akash Kava <39438041+ackava@users.noreply.github.com>
2024-01-21 22:26:27 +0530

    Endpoint fixed

--- src/socket/namespaces/LiveTraceNamespace.ts
+++ src/socket/namespaces/LiveTraceNamespace.ts
@@ -1,8 +1,13 @@
+import { IClassOf } from "@entity-access/entity-access/dist/decorators/IClassOf.js";
 import { RegisterSingleton } from "@entity-access/entity-access/dist/di/di.js";
-import SocketNamespace, { Send } from "@entity-access/server-pages/dist/socket/SocketNamespace.js";
+import SocketNamespace, { Send, SocketNamespaceClient } from "@entity-access/server-pages/dist/socket/SocketNamespace.js";
 
 @RegisterSingleton
 export default class LiveTraceNamespace extends SocketNamespace {
+
+    get clientClass() {
+        return SocketNamespaceClient;
+    }
 
     @Send
     send(...trace: any[]) {
