98cd9e431426391d9f0e63854089e03d954e5413
Akash Kava <39438041+ackava@users.noreply.github.com>
2024-01-30 18:29:53 +0530

    vb

--- src/seed/ui/seed-ui.ts
+++ src/seed/ui/seed-ui.ts
@@ -5,6 +5,6 @@ export default async function seedUI(config: DBConfig) {
     await config.save(UIPackageConfig, {
         package: "@entity-access/tracer-client",
         view: "dist/web/AppIndex",
-        version: "1.0.10"
+        version: "1.0.11"
     });
 }
